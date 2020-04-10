f8873d37d13bc51ffaeca5f189d3588fd6828a9f
uday praveen <praveenuday24@gmail.com>
2020-04-10 14:23:36 +0530

    Update AccntDetails.js
    Initial Commit

--- src/components/AccntTypeOverview/AccntDetails/AccntDetails.js
+++ src/components/AccntTypeOverview/AccntDetails/AccntDetails.js
@@ -2,7 +2,7 @@ import React ,{Component}from "react";
 
 const AccntDetails =({accntDetailsData})=>{
     console.log(accntDetailsData);
-    let details=accntDetailsData.Contributions;
+    let details=accntDetailsData.Exchange;
 
         return(
             <div>
